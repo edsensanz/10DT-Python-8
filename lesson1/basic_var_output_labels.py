dc9191e83416e6c1791a0ad8149ad76b287761d1
JunieMhar <103608612+JunieMhar@users.noreply.github.com>
2022-08-25 09:56:38 +1200

    Add files via upload

---
+++ lesson1/basic_var_output_labels.py
@@ -0,0 +1,18 @@
+# J M Bongcog   
+# Varialbles, output, labels
+
+# Declaration of Variables
+name = "elephant"
+numLegs = 4
+height = 4.5
+hasTail = True
+
+# Output of variables to screen
+print(name)
+print(numLegs)
+print(height)
+print(hasTail)
+
+# Output of variables with labels to screen
+print("The",name,"is one of the big five animals in Africa.")
+print("The elphants has", numLegs,"big legs.")
