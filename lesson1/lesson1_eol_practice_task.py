a160d1d2c563f07eb8cd0476ecee25f8de0d4c8d
JunieMhar <103608612+JunieMhar@users.noreply.github.com>
2022-09-01 09:22:24 +1200

    Add files via upload

---
+++ lesson1/lesson1_eol_practice_task.py
@@ -0,0 +1,24 @@
+# J M Bongcog
+# Focus  -  variables, labels, comments, data types.
+
+# Variables
+
+
+# A summary of this information should be printed out to the screen.
+# output = print()
+
+
+# Clarify the problem
+# name (var)
+
+
+
+# height  -  expressed in metres
+
+
+# address
+# street number, street name, suburb, town, city,
+
+
+# phone number  -  landline or mobile
+# stored as a string  - no calc
